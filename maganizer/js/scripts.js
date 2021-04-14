c30d1421fff754c341b38574db9f3850cff2a2c0
Maksim <maksim.mystafa@gmail.com>
2021-04-14 14:55:02 +0300

    Inputs html validation is added

--- maganizer/js/scripts.js
+++ maganizer/js/scripts.js
@@ -8,6 +8,16 @@ $(function () {
     });
 });
 
-
+$(function () {
+    $("#btnSignUp").click(function () {
+        var password = $("#password").val();
+        var confirmPassword = $("#confirmPassword").val();
+        if (password != confirmPassword) {
+            alert("Passwords do not match.");
+            return false;
+        }
+        return true;
+    });
+});
 
 
